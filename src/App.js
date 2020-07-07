09eed0d892493c6a040b58fa7cf7e9c1ccb3ed59
Muzna Ali <Muzna.wajidali@gmail.com>
2020-07-07 12:06:47 +0500

    Material ui react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Barchart from "./Barchart";
+import AppBar from "./appbar";
+import { makeStyles } from '@material-ui/core/styles';
+import{ Paper, TextField ,Button ,CircularProgress} from '@material-ui/core';
 
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  Paper:{height:350,
+  width:350,
+  margin:"0 auto",
+marginTop: 30,
+textAlign:"center",
+padding:15
+},
+input:{width :"95%",
+marginBottom:10
+}
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={classes.root}>
+   <AppBar />
+    <Paper elevation={3} className={classes.Paper}>
+    <h1><center><Avatar src="/broken-image.jpg" ></Avatar>
+    </center> Login Form Using React </h1>
+    <form className={classes.root} noValidate autoComplete="off">
+  <TextField   id="standard-basic" label="Enter User Email" className={classes.input}/>
+  <TextField id="standard-basic" label="Enter Password" className={classes.input}/></form>
+  <Button variant="contained" color="primary"className={classes.input}>
+  Log-In
+</Button>
+<CircularProgress value={50} />
+</Paper>
+<Barchart />
+  </div>
   );
 }
 
